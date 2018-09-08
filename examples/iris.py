e3066e51e02cebc1263ce2ae18b688e66d109a66
Kyle Safran <kylesafran@Kyles-MacBook-Air.local>
2018-09-08 14:18:06 -0400

    save model

--- examples/iris.py
+++ examples/iris.py
@@ -1,29 +1,49 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import pystan
+import pickle
 
-iris_file = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+def get_iris_dataset():
+    iris_file = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 
-iris_data = pd.read_csv(iris_file,
-                        names=['sepal_length',
-                               'sepal_width',
-                               'petal_length',
-                               'petal_width',
-                               'species'])
+    return pd.read_csv(iris_file,
+                            names=['sepal_length',
+                                   'sepal_width',
+                                   'petal_length',
+                                   'petal_width',
+                                   'species'])
 
-# Simple multi-level model, predict petal length using sepal length
-# and random effects from each species.
-species = LabelEncoder()
-iris_data['species_num'] = species.fit_transform(iris_data['species']) + 1
+def format_stan_data(iris_data):
+    # Simple multi-level model, predict petal length using sepal length
+    # and random effects from each species.
+    species = LabelEncoder()
+    iris_data['species_num'] = species.fit_transform(iris_data['species']) + 1
 
-stan_iris_data = {
-    'n': iris_data.shape[0],
-    'n_species': max(iris_data['species_num']),
-    'sepal_length': iris_data['sepal_length'],
-    'species': iris_data['species_num'],
-    'petal_length': iris_data['petal_length']
-}
+    stan_iris_data = {
+        'n': iris_data.shape[0],
+        'n_species': max(iris_data['species_num']),
+        'sepal_length': iris_data['sepal_length'],
+        'species': iris_data['species_num'],
+        'petal_length': iris_data['petal_length']
+    }
+    return stan_iris_data
 
-iris_model = pystan.StanModel(file='examples/iris.stan')
-iris_fit = iris_model.sampling(data=stan_iris_data, iter=1000,
-                          chains=4)
+def train_iris_model(data, output_file):
+    iris_model = pystan.StanModel(file='examples/iris.stan')
+    iris_fit = iris_model.sampling(data=data, iter=1000,
+                              chains=4)
+
+    # you must pickle the stan model before the stan fit
+    with open(output_file, 'wb') as f:
+        pickle.dump({'model':iris_model,
+                     'fit':iris_fit}, f)
+    return iris_fit
+
+def main():
+    iris_data = get_iris_dataset()
+    stan_iris_data = format_stan_data(iris_data)
+    train_iris_model(stan_iris_data, 'tmp/iris.pkl')
+
+if __name__ == '__main__':
+    main()
+
