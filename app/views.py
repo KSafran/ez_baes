ab1e43d31c91cbac179cf21157728171db6e0d43
Kyle Safran <kylesafran@Kyles-MacBook-Air.local>
2018-09-08 17:06:35 -0400

    landing page

--- app/views.py
+++ app/views.py
@@ -3,6 +3,10 @@ from . import iris
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello():
+    return 'Welcome to EZ Baes'
+
+@app.route('/train_iris')
+def train_iris():
     iris.fit_iris()
     return 'Training Model...'
